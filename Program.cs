988855477a60db1a4844000f84e27d2534b672a7
AntNov <tonianovak@yahoo.com>
2023-04-26 12:42:50 +0300

    Added an initial code for a preparation part

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+// Preparation
+List<string> wordsList = new List<string>(File.ReadAllLines("words_list.txt"));
+
+Console.WriteLine("Enter a sentence:");
+string input = Console.ReadLine();
+
+List<string> words = SplitWords(input);
+List<string> misspelledWords = FindMisspelledWords(words, wordsList);
+
+static List<string> SplitWords(string input)
+{
+    // Split the input into words and remove punctuation marks
+}
+
+static List<string> FindMisspelledWords(List<string> words, List<string> wordsList)
+{
+    // Check each word for its presence in the wordsList and return a list of misspelled words
+}
