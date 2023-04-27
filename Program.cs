c4623ac22bfccb7a0ead2808642a5f264c537a66
vladshcherbyna <vshcherbyna@kse.org.ua>
2023-04-27 13:14:19 +0300

    first-function

--- Program.cs
+++ Program.cs
@@ -1,21 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
+﻿//using System;
+//using System.Collections.Generic;
 
 // Preparation
 List<string> wordsList = new List<string>(File.ReadAllLines("words_list.txt"));
 
 Console.WriteLine("Enter a sentence:");
-string input = Console.ReadLine();
+string? input = Console.ReadLine();
 
 List<string> words = SplitWords(input);
-List<string> misspelledWords = FindMisspelledWords(words, wordsList);
+//List<string> misspelledWords = FindMisspelledWords(words, wordsList);
 
-static List<string> SplitWords(string input)
+static List<string> SplitWords(string? input)
 {
-    // Split the input into words and remove punctuation marks
+    var withoutPunctuation = new string(input.Where(c => !char.IsPunctuation(c)).ToArray());
+    
+    var words = withoutPunctuation.Split(new[] {' '}, StringSplitOptions.RemoveEmptyEntries);
+    
+    var wordsList = words.ToList();
+    return wordsList;
 }
 
-static List<string> FindMisspelledWords(List<string> words, List<string> wordsList)
+//static List<string> FindMisspelledWords(List<string> words, List<string> wordsList)
 {
     // Check each word for its presence in the wordsList and return a list of misspelled words
 }
